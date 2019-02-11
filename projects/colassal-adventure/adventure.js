b2e7b0055f22be8c1a70dfb69697fdbf18c77c2d
Amy Knudson <amylknudson@gmail.com>
2019-02-11 09:56:17 -0700

    added loot items to enemies, min requirements done with dj-transitions

--- projects/colassal-adventure/adventure.js
+++ projects/colassal-adventure/adventure.js
@@ -6,28 +6,32 @@ let enemies = [
         health: 50,
         strength: 10,
         weakness: "silver stakes",
-        reward: 25
+        reward: 25,
+        bounty: "Amulet of Healing"
     },
     {
         name: "dementors",
         health: 75,
         strength: 15,
         weakness: "patronus spell",
-        reward: 35
+        reward: 35,
+        bounty: "Chocolate"
     },
     {
         name: "voldermort",
         health: 100,
         strength: 20,
         weakness: "elder wand",
-        reward: 50
+        reward: 50,
+        bounty: "Peace"
     },
     {
         name: "Shia Lebouf",
         health: 125,
         strength: 25,
         weakness: "axe",
-        reward: 75
+        reward: 75,
+        bounty: "Shia's decapitated head"
     }
 ]
 
@@ -35,7 +39,7 @@ let playerInfo = {
     health: 100,
     weapons: ["stick"],
     expLvl: 0,
-    enemiesToDefeat: enemies
+    loot: []
 }
 
 
@@ -60,7 +64,7 @@ while(playerInfo.health > 0 && playerInfo.expLvl <= 100){
 function walk(){
     gainExp()
     let chance = Math.floor(Math.random() * 3);
-    if(chance <= 2){
+    if(chance === 2){
         getAttacked()
     }
 }
@@ -89,13 +93,13 @@ function getAttacked(){
 
 function gainExp(){
     playerInfo.expLvl += 5;
-    if(playerInfo.expLvl === 10){
+    if(playerInfo.expLvl >= 10 && !playerInfo.weapons.includes("silver stakes")){
         playerInfo.weapons.push("silver stakes")
-    } else if( playerInfo.expLvl === 20){
+    } else if( playerInfo.expLvl >= 20 && !playerInfo.weapons.includes("patronus spell")){
         playerInfo.weapons.push("patronus spell")
-    } else if( playerInfo.expLvl === 30){
+    } else if( playerInfo.expLvl === 30 && !playerInfo.weapons.includes("elder wand")){
         playerInfo.weapons.push("elder wand")
-    } else if( playerInfo.expLvl === 30){
+    } else if( playerInfo.expLvl === 30 && !playerInfo.weapons.includes("axe")){
         playerInfo.weapons.push("axe")
     }
 }
@@ -107,29 +111,40 @@ function enemySelector(){
 function weaponSelector(){ 
     return playerInfo.weapons[Math.floor(Math.random()*playerInfo.weapons.length)]
 }
+function hitStrength(){
+    return Math.floor(Math.random()*15)+5
+}
+
+function victory(enemy, weapon){
+    console.log(`You defeated ${enemy.name} with one blow using ${weapon}! ${weapon} was destroyed in the process. 
+    You gained ${enemy.reward} experience points and ${enemy.bounty} from this battle.`);
+    playerInfo.expLvl += enemy.reward;
+    playerInfo.loot.push(enemy.bounty)
+    enemies.splice(enemies.indexOf(enemy.name), 1);
+    playerInfo.weapons.splice(playerInfo.weapons.indexOf(weapon), 1);
+}
 
 function fight(enemy, weapon){
     if(weapon === enemy.weakness){
-        enemies.splice(enemies.indexOf(enemy.name), 1)
-        playerInfo.weapons.splice(playerInfo.weapons.indexOf(weapon), 1)
-        playerInfo.expLvl += enemy.reward
-        console.log(`You defeated ${enemy.name} with one blow using ${weapon}! ${weapon} was destroyed in the process. 
-        You gained ${enemy.reward} experience points from this battle.`)
-
+        victory()
     }else {
         while(playerInfo.health > 0 && enemy.health > 0){
-            let hitStrength = Math.floor(Math.random()*15)+5;
-            enemy.health -= hitStrength;
-            console.log(`You hit ${enemy.name} with ${hitStrength}`)
-            playerInfo.health -= enemy.strength;
+            let damageGiven = hitStrength();
+            let damageTaken = hitStrength();
+            enemy.health -= damageGiven;
+            console.log(`You hit ${enemy.name} with ${damageGiven}`)
+            playerInfo.health -= damageTaken;
+            console.log(`You took a hit of ${damageTaken}, your health is now ${playerInfo.health}`)
             if(enemy.health <= 0){
                 playerInfo.expLvl += enemy.reward;
-                playerInfo.health += 20;
+                playerInfo.health += 50;
                 console.log(`You've defeated ${enemy.name}! Your health is now ${playerInfo.health} and 
-                you gained ${enemy.reward} experience points from this battle.`)
+                you gained ${enemy.reward} experience points and ${enemy.bounty} from this battle.`)
+                enemies.splice(enemies.indexOf(enemy.name), 1);
             } 
             else if(playerInfo.health <= 0){
                 console.log(`${playerInfo.name} was defeated by ${enemy.name}`)
+                console.log("GAME OVER, MAN. GAME OVER.")
             }
         }
     }
