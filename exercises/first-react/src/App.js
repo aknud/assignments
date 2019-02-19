547e005fb5f3d385f14682539a15956fe7a33a2f
Amy Knudson <amylknudson@gmail.com>
2019-02-19 16:33:20 -0700

    file clean up and started react app

---
+++ exercises/first-react/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "./components/Nav";
+import Products from "./components/Products";
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+        <h1>Hey Now! I am in the App.js file</h1>
+      <Products />
+    </div>
+  )
+}
+
+
+
+export default App;
