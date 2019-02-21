f52eed708725481a4901ed59a11cfb77f9f5dace
Amy Knudson <amylknudson@gmail.com>
2019-02-20 18:48:52 -0700

    set up sass and did minor styling to nav

---
+++ exercises/first-react/src/components/LowerNav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const LowerNav = () => {
+    return (
+        <div className="lowerNav">
+            <ul className="lowerNavLinks">
+                <li>Women</li>
+                <li>Men</li>
+                <li>Kids</li>
+                <li>Departments</li>
+                <li>Brands</li>
+                <li>Sale</li>
+            </ul>
+            <button>Sign In / Register</button>
+        </div>
+    )
+}
+
+export default LowerNav
--- exercises/first-react/src/components/Nav.js
+++ exercises/first-react/src/components/Nav.js
@@ -1,10 +1,20 @@
 import React from "react";
+import LowerNav from "./LowerNav";
 
 export default class Nav extends React.Component {
     render(){
         return (
-            <div>
-                <h1>Nav Bar</h1>
+            <div className="nav" >
+                <img src="https://m.media-amazon.com/images/G/01/zappos/melody/zapposPBS._CB1509642213_.svg" alt="Welcome! Go to Zappos.com Homepage!" className="z_icon"/>
+                <form action="/search" method="get">
+                    <label htmlFor="search">Search</label>
+                    <input type="search" name="nav-search" id="search" placeholder="Search for shoes, clothes, etc."/>
+                    <button type="submit">Search</button>
+                </form>
+                <div>
+                    <button>My Cart</button>
+                </div>
+                <LowerNav />
             </div>
         )
     }
