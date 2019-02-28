7ff0a0bc91fe348f4e60a8769b0a779b0fa28e60
Amy Knudson <amylknudson@gmail.com>
2019-02-27 22:35:02 -0700

    hit-list finished

---
+++ exercises/React-Exercises/hit-list/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      arr: []
+    }
+  }
+  async componentDidMount(){
+      let list = await axios.get("http://s3.amazonaws.com/v-school/data/hitlist.json")
+      list.data[5].image = "https://pbs.twimg.com/media/DMsGz80VQAAeqfw.jpg";
+      this.setState({ arr: list.data})
+      console.log(list.data)
+  }
+
+  render() {
+    console.log(111,this.state.arr)
+    let mappedArr = this.state.arr.map((item, i) => {
+      return (
+        <div className="target" key={i + Math.random()}>
+          <img style={{height: 300, width: 350, border: "1px solid black"}} src={item.image} alt=""/>
+          <h1 className="target-title">{item.name}</h1>
+        </div>
+      )
+    })
+    return (
+      <div className="main">
+        <nav className="nav">
+          <img style={{height: 100, width: 200}} src="https://cdn.shopify.com/s/files/1/1877/8853/files/Don-Vito_grande.jpg?v=1501127874" alt=""/> 
+          <h1>Don Corleone's Hit List</h1> 
+          </nav>
+        <div className="target-container">
+          {mappedArr}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
