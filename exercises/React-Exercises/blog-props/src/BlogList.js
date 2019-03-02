c63376d9916cdbe3e4ea26361964b9742ba5bb16
Amy Knudson <amylknudson@gmail.com>
2019-03-01 18:23:20 -0700

    mobile view finished

--- exercises/React-Exercises/blog-props/src/BlogList.js
+++ exercises/React-Exercises/blog-props/src/BlogList.js
@@ -1,11 +1,17 @@
 import React from "react";
 import BlogPost from "./BlogPost";
 
+
 const BlogList = () => {
     return (
         <div>
-            <h1>BlogList</h1>
             <BlogPost />
+            <div className="btn-container">
+                <button className="more-posts-btn">
+                    Older Posts 
+                    <i class='fas fa-long-arrow-alt-right'></i>
+                </button>
+            </div>
         </div>
     )
 }
--- exercises/React-Exercises/blog-props/src/BlogPost.js
+++ exercises/React-Exercises/blog-props/src/BlogPost.js
@@ -1,9 +1,19 @@
 import React from "react";
+import blogs from "./blogs.json";
 
 const BlogPost = () => {
+    let mappedBlogs = blogs.map(blog => {
+        return (
+            <div className="blog-container">
+                <h1>{blog.title}</h1>
+                <h2>{blog.subtitle}</h2>
+                <p>Posted by <b>Start Bootstrap</b> on <span className="date">{blog.date}</span></p>
+            </div>
+        )
+    })
     return (
         <div>
-            <h1>BlogPost</h1>
+            {mappedBlogs}
         </div>
     )
 }
