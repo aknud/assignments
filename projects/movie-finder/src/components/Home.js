3a4695969c2fcea80aa490c5eee654613ce6c82d
Amy Knudson <amylknudson@gmail.com>
2019-03-08 17:07:22 -0700

    styles finished for home view

--- projects/movie-finder/src/components/Home.js
+++ projects/movie-finder/src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
 
     return (
         <div className="Home">
-            <h1 className="home-message">Get all the information for your favorite movies, even though you could’ve just googled it…But here you are.</h1>
+            <h1 className="home-message">Get all the information for your favorite movies, even though you could’ve just googled it… <br/> But here you are.</h1>
             <Link to="/search"><button>Yeah I'm here already, might as well try it out.</button></Link>
             <button onClick={redirectPage}>Wait, this isn't google? Take me to google where all my wishes are granted.</button>
         </div>
